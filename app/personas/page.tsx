1f460643436fd8fc197131fb51a8cdefbb3734f1
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2024-05-11 16:35:47 -0500

    table

--- app/personas/page.tsx
+++ app/personas/page.tsx
@@ -1,11 +1,33 @@
 'use client'
 import { getPersons } from "@/api/api";
+import MainTable from "@/components/main_table";
 import { Persona } from "@/interfaces/personas";
 import { useEffect, useState } from "react";
+
+
 
 const PersonasPage = () => {
 
     const [personas, setPersonas] = useState<Persona[]>([]);
+
+    const columns = [
+        {
+            accessorKey: 'DocNumber',
+            header: ()=><span>Doc. Identidad</span>
+        },
+        {
+            accessorKey: 'Name',
+            header: ()=><span>Nombres</span>
+        },
+        {
+            accessorKey: 'MiddleName',
+            header: ()=><span>Ap. Paterno</span>
+        },
+        {
+            accessorKey: 'LastName',
+            header: ()=><span>Ap. Materno</span>
+        },
+    ]
 
     const cargaData = async ()=>{
         setPersonas(await  getPersons());
@@ -24,7 +46,11 @@ const PersonasPage = () => {
     return (
         <div>
         <h1>Personas</h1>
+        <div className="w-full">
+            <MainTable<Persona> data={personas} columns={columns}  />
         </div>
+        </div>
+        
     );
 }
 
--- components/main_table.tsx
+++ components/main_table.tsx
@@ -1,102 +1,268 @@
 'use client'
 
 import {
-    Column,
-    ColumnDef,
-    ColumnFiltersState,
-    FilterFn,
-    SortingFn,
-    flexRender,
-    getCoreRowModel,
-    getFilteredRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    sortingFns,
-    useReactTable,
+  Column,
+  ColumnDef,
+  ColumnFiltersState,
+  FilterFn,
+  SortingFn,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  sortingFns,
+  useReactTable,
 } from '@tanstack/react-table'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import {
-    RankingInfo,
-    rankItem,
-    compareItems,
+  RankingInfo,
+  rankItem,
+  compareItems,
 } from '@tanstack/match-sorter-utils'
 
 declare module '@tanstack/react-table' {
-    //add fuzzy filter to the filterFns
-    interface FilterFns {
-        fuzzy: FilterFn<unknown>
-    }
-    interface FilterMeta {
-        itemRank: RankingInfo
-    }
+  //add fuzzy filter to the filterFns
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo
+  }
 }
 
 // Define a custom fuzzy filter function that will apply ranking info to rows (using match-sorter utils)
 const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-    // Rank the item
-    const itemRank = rankItem(row.getValue(columnId), value)
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value)
 
-    // Store the itemRank info
-    addMeta({
-        itemRank,
-    })
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  })
 
-    // Return if the item should be filtered in/out
-    return itemRank.passed
+  // Return if the item should be filtered in/out
+  return itemRank.passed
 }
 
 // Define a custom fuzzy sort function that will sort by rank if the row has ranking information
 const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
-    let dir = 0
+  let dir = 0
 
-    // Only sort by rank if the column has ranking information
-    if (rowA.columnFiltersMeta[columnId]) {
-        dir = compareItems(
-            rowA.columnFiltersMeta[columnId]?.itemRank!,
-            rowB.columnFiltersMeta[columnId]?.itemRank!
-        )
-    }
+  // Only sort by rank if the column has ranking information
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank!,
+      rowB.columnFiltersMeta[columnId]?.itemRank!
+    )
+  }
 
-    // Provide an alphanumeric fallback for when the item ranks are equal
-    return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
+  // Provide an alphanumeric fallback for when the item ranks are equal
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
 }
 
 const MainTable = <T,>(
-    { data, columns }: {
-        data: T[],
-        columns: ColumnDef<T>[]
+  { data, columns }: {
+    data: T[],
+    columns: ColumnDef<T>[]
 
-    }
+  }
 ) => {
 
-    const [globalFilter, setGlobalFilter] = useState('')
+  const [globalFilter, setGlobalFilter] = useState('')
 
-    // const [array, setArray] = useState<T[]>([]);
-    const table = useReactTable({
-        data,
-        columns,
-        filterFns: {
-            fuzzy: fuzzyFilter, //define as a filter function that can be used in column definitions
-        },
-        state: {
-            //   columnFilters,
-            globalFilter,
-        },
-        // onColumnFiltersChange: setColumnFilters,
-        onGlobalFilterChange: setGlobalFilter,
-        globalFilterFn: 'fuzzy', //apply fuzzy filter to the global filter (most common use case for fuzzy filter)
-        getCoreRowModel: getCoreRowModel(),
-        getFilteredRowModel: getFilteredRowModel(), //client side filtering
-        getSortedRowModel: getSortedRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        // debugTable: true,
-        debugHeaders: true,
-        debugColumns: false,
-    })
+  // const [array, setArray] = useState<T[]>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    filterFns: {
+      fuzzy: fuzzyFilter, //define as a filter function that can be used in column definitions
+    },
+    state: {
+      //   columnFilters,
+      globalFilter,
+    },
+    // onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: 'fuzzy', //apply fuzzy filter to the global filter (most common use case for fuzzy filter)
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(), //client side filtering
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    // debugTable: true,
+    // debugHeaders: true,
+    // debugColumns: false,
+  })
 
-    return (<>
-
-    </>)
+  return (<>
+  <DebouncedInput
+          value={globalFilter ?? ''}
+          onChange={value => setGlobalFilter(String(value))}
+          className="p-2 font-lg shadow border border-block"
+          placeholder="Search all columns..."
+        />
+    <table className='w-full table'>
+      <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => {
+              return (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <>
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : '',
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: ' 🔼',
+                          desc: ' 🔽',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    </>
+                  )}
+                </th>
+              )
+            })}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map(row => {
+          return (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => {
+                return (
+                  <td key={cell.id}>
+                    {flexRender(
+                      cell.column.columnDef.cell,
+                      cell.getContext()
+                    )}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+    <div className="h-2" />
+    <div className="flex items-center gap-2">
+      <button
+        className="border rounded p-1"
+        onClick={() => table.setPageIndex(0)}
+        disabled={!table.getCanPreviousPage()}
+      >
+        {'<<'}
+      </button>
+      <button
+        className="border rounded p-1"
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+      >
+        {'<'}
+      </button>
+      <button
+        className="border rounded p-1"
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+      >
+        {'>'}
+      </button>
+      <button
+        className="border rounded p-1"
+        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+        disabled={!table.getCanNextPage()}
+      >
+        {'>>'}
+      </button>
+      <span className="flex items-center gap-1">
+        <div>Page</div>
+        <strong>
+          {table.getState().pagination.pageIndex + 1} of{' '}
+          {table.getPageCount()}
+        </strong>
+      </span>
+      <span className="flex items-center gap-1">
+        | Go to page:
+        <input
+          type="number"
+          defaultValue={table.getState().pagination.pageIndex + 1}
+          onChange={e => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0
+            table.setPageIndex(page)
+          }}
+          className="border p-1 rounded w-16"
+        />
+      </span>
+      <select
+        value={table.getState().pagination.pageSize}
+        onChange={e => {
+          table.setPageSize(Number(e.target.value))
+        }}
+      >
+        {[10, 20, 30, 40, 50].map(pageSize => (
+          <option key={pageSize} value={pageSize}>
+            Show {pageSize}
+          </option>
+        ))}
+      </select>
+    </div>
+  </>)
 }
+
+function Filter({ column }: { column: Column<any, unknown> }) {
+  const columnFilterValue = column.getFilterValue()
+
+  return (
+    <DebouncedInput
+      type="text"
+      value={(columnFilterValue ?? '') as string}
+      onChange={value => column.setFilterValue(value)}
+      placeholder={`Search...`}
+      className="w-36 border shadow rounded"
+    />
+  )
+}
+
+// A typical debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number
+  onChange: (value: string | number) => void
+  debounce?: number
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input {...props} value={value} onChange={e => setValue(e.target.value)} />
+  )
+}
+
+export default MainTable
